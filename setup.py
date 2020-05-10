e3f3cf48dec87f1c0d4b5bb100727ab6657d0bbe
Eric Lundquist <ericlundquist@EricLundquist.local>
2020-05-10 15:31:20 -0700

    added setuptools build script

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='rankfm',
+    version='0.1.0',
+    description='a python implementation of the generic factorization machines model class '
+                'adapted for collaborative filtering recommendation/ranking problems '
+                'with implicit feedback user-item interaction data',
+    author='Eric Lundquist',
+    author_email='e.t.lundquist@gmail.com',
+    url='https://github.com/etlundquist/rankfm',
+    keywords=['machine', 'learning', 'recommendation', 'factorization', 'machines', 'implicit'],
+    license='GNU General Public License v3.0',
+    packages=['rankfm'],
+    python_requires='>=3.6',
+    install_requires=['numpy', 'pandas']
+)
+
