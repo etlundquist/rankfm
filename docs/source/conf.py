22f9b8c89b712bc311b80a614c7b0b82dd547eff
Eric Lundquist <ericlundquist@EricLundquist.local>
2020-06-14 21:04:33 -0700

    typo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('../..'))
-autodoc_mock_imports = ["rankfm/_rankfm"]
+autodoc_mock_imports = ["rankfm._rankfm"]
 
 # import dependencies
 # -------------------
