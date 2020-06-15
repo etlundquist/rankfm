9bc6936ff8ac373df99e7156373d099ae94cd439
Eric Lundquist <ericlundquist@EricLundquist.local>
2020-06-15 08:33:08 -0700

    downgrade sphinx options

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,10 +52,8 @@ extensions = [
 
 # add autodoc default options
 autodoc_default_options = {
-    'members': True,
     'member-order': 'bysource',
     'special-members': '__init__',
-    'undoc-members': True,
     'exclude-members': '__weakref__'
 }
 
